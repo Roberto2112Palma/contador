db0c15ba6a89f1fe1a02ba933e98eac262f54573
Roberto2112Palma <robertopalmaruiz@fp.iesromerovargas.com>
2022-03-07 19:04:08 +0100

    Actualziamos fichero app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import locale
 app = Flask(__name__)
 locale.setlocale(locale.LC_TIME, '')
 
+
 @app.route("/")
 def index():
-    
     # Load current count
     f = open("count.txt", "r")
     count = int(f.read())
@@ -21,21 +21,18 @@ def index():
     f.write(str(count))
     f.close()
 
-    # Render HTML with count variable
-    #render_template("index.html", count=count)
-
     the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
 
     # Render HTML with variable
-    return render_template("index.html", the_time=the_time, tema="dogs", count=count )
+    return render_template(
+        "index.html", the_time=the_time, tema="dogs", count=count)
 
     # return f"""
     # <h1>Hello heroku</h1>
     # <p>It is currently {the_time}.</p>
     # <img src="http://loremflickr.com/600/400" />
     # """
- 
+
 
 if __name__ == "__main__":
     app.run()
-
