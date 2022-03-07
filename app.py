55128ec89b45c5de7c0680da35a5e84b14bdefaa
Roberto2112Palma <robertopalmaruiz@fp.iesromerovargas.com>
2022-03-07 18:23:22 +0100

    Añadimos el nuevo programa

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
-#Comentario
+from datetime import datetime
+import locale
+
 app = Flask(__name__)
+locale.setlocale(locale.LC_TIME, '')
 
 @app.route("/")
 def index():
@@ -19,7 +22,20 @@ def index():
     f.close()
 
     # Render HTML with count variable
-    return render_template("index.html", count=count)
+    render_template("index.html", count=count)
+
+    the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
+
+    # Render HTML with variable
+    return render_template("index.html", the_time=the_time, tema="dogs" )
+
+    # return f"""
+    # <h1>Hello heroku</h1>
+    # <p>It is currently {the_time}.</p>
+    # <img src="http://loremflickr.com/600/400" />
+    # """
+ 
 
 if __name__ == "__main__":
     app.run()
+
